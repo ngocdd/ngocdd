170cc89ebcdb8e29751e8e6ae5eb32546b5c5215
“ngocdd” <ngocdd94@gmail.com git config --global user.name ngocddgit config --global user.email ngocdd94@gmail.com>
2023-09-30 01:02:11 +0700

    updates

--- next.config.js
+++ next.config.js
@@ -2,13 +2,15 @@
 
 // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
 const nextConfig = {
-  webpack: config => {
-    const oneOfRule = config.module.rules.find(rule => rule.oneOf);
+  webpack: (config) => {
+    const oneOfRule = config.module.rules.find((rule) => rule.oneOf);
 
     // Next 12 has multiple TS loaders, and we need to update all of them.
-    const tsRules = oneOfRule.oneOf.filter(rule => rule.test && rule.test.toString().includes('tsx|ts'));
+    const tsRules = oneOfRule.oneOf.filter(
+      (rule) => rule.test && rule.test.toString().includes("tsx|ts")
+    );
 
-    tsRules.forEach(rule => {
+    tsRules.forEach((rule) => {
       // eslint-disable-next-line no-param-reassign
       rule.include = undefined;
     });
@@ -17,14 +19,18 @@ const nextConfig = {
   },
   compress: true,
   generateEtags: true,
-  pageExtensions: ['tsx', 'mdx', 'ts'],
+  pageExtensions: ["tsx", "mdx", "ts"],
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: false,
   images: {
-    domains: ['images.unsplash.com', 'source.unsplash.com'],
+    domains: [
+      "images.unsplash.com",
+      "source.unsplash.com",
+      "ngocdd.qablogs.io.vn",
+    ],
   },
 };
 
